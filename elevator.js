dbe4f5ef20dbc74e01aa2409e90d21c17c2bf938
Eun Joo <eunjoolee787@gmail.com>
2014-12-19 14:31:25 -1000

    finish assignment

--- elevator.js
+++ elevator.js
@@ -4,12 +4,33 @@
 module.exports = {
 	goto: function (currentFloor, destination){
 		var destinationNumber = parseInt(destination);
+    // validates destinationNumber
+    if(destinationNumber > 3) {
+      return 0;
+    } 
+    if(typeof currentFloor === "string") {
+      return 0;
+    }
+    if(typeof destination === "number") {
+      return 0;
+    }
+    if(currentFloor > 3) {
+      return 0;
+    }
+    if(destinationNumber === currentFloor) {
+      return 0;
+    } 
+    if(currentFloor instanceof Array) {
+      return 0;
+    }
+    if(currentFloor < destinationNumber) {
+      return destinationNumber - currentFloor;
+    }
+    if(currentFloor > destinationNumber) {
+      return destinationNumber - currentFloor;
+    }
 
-
-		if(destination > currentFloor){
-			return parseInt (destination);
-		}
-	// return ;	
+    return 0;
 	}
 };
 
